b6a7b2d1e49da64402e7d93d5bdf4c9bd0f00c8c
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-06-11 10:04:42 +0200

    Added SQL insert

--- calculator.php
+++ calculator.php
@@ -4,37 +4,59 @@
 <body>
 
 	<?php	
-	if(empty($_GET["valueA"]) || empty($_GET["valueB"]) ){
-
-		echo "<div>Cannot calc null input </div>";
+	if (empty($_GET["valA"]) || empty($_GET["valB"]))
+	{
+		die("Empty value(s)");
 	}
 
 	else
 	{
-		$a =$_GET["valueA"];
-		$b =$_GET["valueB"];
-		$e = $_GET["box"];
-		$r = $_GET["age"];
-		$v = $a+$b;
-		$d = $a/$b;
-		$n = $a*$b;
-		$m = $a-$b;
+		$a=$_GET["valA"];
+		$b=$_GET["valB"];
+		$v=$a+$b;
+		$d=$a/$b;
+		$n=$a*$b;
+		$m=$a-$b;
 
-		if(($_GET["age"]) == "basic")
+		if ($_GET["agree"]==off)
 		{
-			echo "<H3>Sucet: ".$a." + ".$b." = ".$v."</H3>";
-			echo "<H3>Podiel: ".$a."/".$b."=".$d."</H3>";		
+			die("You have to agree with calculate");
 		}
 
 		else
 		{
-			echo "<H3>Sucet: ".$a." + ".$b." = ".$v."</H3>";
-			echo "<H3>Podiel: ".$a."/".$b."=".$d."</H3>";
-			echo "<H3>Sucin: ".$a."*".$b."=".$n."</H3>";
-			echo "<H3>Rozdiel: ".$a."-".$b."=".$m."</H3>";
-			echo "Chcek box status ".$_GET["box"]."!";		
-}
-		
+
+		if(($_GET["operations"])=="basic")
+		{
+			echo "<DIV>Sucet: ".$a."+".$b."=".$v."</DIV>";
+			echo "<DIV>Rozdiel: ".$a."-".$b."=".$m."</DIV>";		
+		}
+
+		else
+		{
+			echo "<DIV>Sucet: ".$a."+".$b."=".$v."</DIV>";
+			echo "<DIV>Podiel: ".$a."/".$b."=".$d."</DIV>";
+			echo "<DIV>Sucin: ".$a."*".$b."=".$n."</DIV>";
+			echo "<DIV>Rozdiel: ".$a."-".$b."=".$m."</DIV>";	
+
+		$dotaz="INSERT INTO history (valA,valB) VALUES (".$a.",".$b.")";
+		$conn=mysqli_connect("localhost:3306", "root", "", "phpcalc");
+		if (!$conn)
+		{
+			die("Connection failed: ".mysqli_connector_error());
+		}
+
+		echo "Connected successfully";
+
+		if ($conn->query($dotaz)===TRUE)
+		{
+			echo "New record created successfully";
+		}
+
+		else
+			echo "Error: ".$sql."<BR>".$conn->error;
+		}
+	}
 ?>
 </body>
 </html>
